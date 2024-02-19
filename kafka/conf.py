968a97efea9ea70af86625ef70ef91524b844f1e
Omid Mohkamkar <omidmk678@gmail.com>
2024-02-19 19:33:09 +0330

    Add files via upload

---
+++ kafka/conf.py
@@ -0,0 +1,71 @@
+server_urls = {
+    'uk': 'http://example.com',
+    'de': 'http://example1.com',
+    'ca': 'http://example2.com',
+}
+
+server_credentials = {
+    'username': 'admin',
+    'password': 'admin',
+}
+
+arvan_host = 'example.ninja'
+cf_host = 'example.com.tech'
+
+links = {
+    'hamrah-aval': {
+        'address': 'hamrah.bigomidmk.tech:443',
+        'suffix': '1',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'irancell': {
+        'address': 'mtn.bigomidmk.tech:443',
+        'suffix': '2',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'rightel': {
+        'address': 'rtl.bigomidmk.tech:443',
+        'suffix': '3',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'mokhaberat': {
+        'address': 'mkh.bigomidmk.tech:443',
+        'suffix': '4',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'shatel': {
+        'title': 'Shatel',
+        'address': 'shatel.bigomidmk.tech:443',
+        'suffix': '5',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'mobin-net': {
+        'address': 'mbt.bigomidmk.tech:443',
+        'suffix': '6',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'pars-online': {
+        'address': 'prs.bigomidmk.tech:443',
+        'suffix': '7',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'hiweb': {
+        'address': 'hwb.bigomidmk.tech:443',
+        'suffix': '8',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+    'asiatech': {
+        'address': 'ast.bigomidmk.tech:443',
+        'suffix': '9',
+        'host': '{}1.' + cf_host,
+        'sni': 'admin1.' + cf_host,
+    },
+}
